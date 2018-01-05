563310d9892ef6a39466c05cb75762502fafce94
AboElnuoR <m.aboelnuor@gmail.com>
2018-01-06 00:19:29 +0200

    fix login issues

--- app/User.php
+++ app/User.php
@@ -26,4 +26,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'User_ID';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
