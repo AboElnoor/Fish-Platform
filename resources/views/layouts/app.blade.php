76d53f01dcefbc38e3d958aa2dbcb6d64c5f4cc8
Adel <mh4md.a@gmail.com>
2018-01-06 05:07:49 +0200

    Login UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,8 +46,8 @@
                     <ul class="nav navbar-nav navbar-left">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}">دخول</a></li>
+                            <li><a href="{{ route('register') }}">تسجيل</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
