8bba5878a0f9f26f027694690c94be809b31a155
Adel <mh4md.a@gmail.com>
2018-01-06 05:31:52 +0200

    Home buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+<section class="home">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <a href="#" class="box-anchor">
+                    <div class="box">
+                        <div class="box-icon">
+                            <i class="fa fa-users fa-2x" aria-hidden="true"></i>
                         </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                        <div class="box-title">
+                            <h4>المستخدمين</h4>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="box-anchor">
+                    <div class="box">
+                        <div class="box-icon">
+                            <i class="fa fa-line-chart fa-2x" aria-hidden="true"></i>
+                        </div>
+                        <div class="box-title">
+                            <h4>مزارع الأسماك</h4>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="box-anchor">
+                    <div class="box">
+                        <div class="box-icon">
+                            <i class="fa fa-cogs fa-2x" aria-hidden="true"></i>
+                        </div>
+                        <div class="box-title">
+                            <h4>مستلزمات الانتاج والمصانع والتجار</h4>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="box-anchor">
+                    <div class="box">
+                        <div class="box-icon">
+                            <i class="fa fa-calendar fa-2x" aria-hidden="true"></i>
+                        </div>
+                        <div class="box-title">
+                            <h4>الجمعيات</h4>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="box-anchor">
+                    <div class="box">
+                        <div class="box-icon">
+                            <i class="fa fa-tachometer fa-2x" aria-hidden="true"></i>
+                        </div>
+                        <div class="box-title">
+                            <h4>الجهات والخدمات</h4>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="box-anchor">
+                    <div class="box">
+                        <div class="box-icon">
+                            <i class="fa fa-sign-out fa-2x" aria-hidden="true"></i>
+                        </div>
+                        <div class="box-title">
+                            <h4>خروج</h4>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
-</div>
+</section>
 @endsection
