ec4bb714e44275e89c457b66e57a9a30ce11679a
Adel <mh4md.a@gmail.com>
2018-01-20 21:24:57 +0200

    Market UI

--- resources/views/markets/create.blade.php
+++ resources/views/markets/create.blade.php
@@ -4,13 +4,16 @@
 
 <section class="form farms">
     <div class="container">
-        <h2 class="section-title">الأسعار</h2>
+        <h2 class="section-title">سوق بشاير : عرض شراء</h2>
         @include('layouts.alert')
 
         <div class="tab-content">
             <div id="home" class="tab-pane fade in active">
                 <h3 class="tab-title">الأسعار</h3>
                 <div class="row">
+                    <div class="col-md-12">
+                        <h4><b>بيانات المحصول</b></h4>
+                    </div>
                     {!!
                         Form::open([
                             'method' => isset($price) ? 'PUT' : 'POST',
@@ -19,110 +22,225 @@
                     !!}
                         <div class="col-md-12">
                             <div class="form-group">
-                                {!! Form::label('HSCode_ID', 'منتج/ كود HS') !!}
-                                {!! Form::select(
-                                        'HSCode_ID',
-                                        $hSCodes->prepend('من فضلك اختار', 0),
-                                        $price->HSCode_ID ?? null,
-                                        ['class' => 'form-control']
-                                    ) !!}
+                                <label for="hsCode">الجهة / الاسم</label>
+                                <select name="hsCode" id="hsCode" class="form-control">
+                                    <option value="1">اول</option>
+                                    <option value="2">تانى</option>
+                                    <option value="3">ثالث</option>
+                                </select>
                             </div>
                         </div>
 
                         <div class="col-md-6">
-                            <div class="form-group">
-                                {!! Form::label('Market_ID', 'المدينة/الســــوق') !!}
-                                {!! Form::select(
-                                        'Market_ID',
-                                        [
-                                            0 => 'من فضلك اختار',
-                                            1 => 'سوق العبور/القاهرة',
-                                            2 => 'سوق الجملة/6 اكتوبر',
-                                            3 => 'سوق الحضرة/الاسكندرية',
-                                            4 => 'سوق الجملة/اسيوط',
-                                            5 => 'سوق الجملة/المنيا',
-                                        ],
-                                        $price->Market_ID ?? null,
-                                        ['class' => 'form-control']
-                                    ) !!}
-                            </div>
+                            <label for="name">اسم العارض</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="name">انجليزى</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="col-md-12">
+                            <label for="name">موبايل العارض</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="name">الايميل</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="name">الفاكس</label>
+                            <input type="text" class="form-control">
                         </div>
 
                         <div class="col-md-6">
                             <div class="form-group">
-                                {!! Form::label('PriceDate', 'تاريخ السعر') !!}
+                                {!! Form::label('PriceDate', 'بداية العرض') !!}
                                 {{ Form::date('PriceDate', \Carbon\Carbon::now(), ['class' => 'form-control']) }}
                             </div>
                         </div>
 
                         <div class="col-md-6">
                             <div class="form-group">
-                                {!! Form::label('PriceMin', 'أدنى سعر') !!}
-                                {!! Form::text('PriceMin', $price->PriceMin ?? null, ['class' => 'form-control']) !!}
+                                {!! Form::label('PriceMin', 'نهاية العرض') !!}
+                                {{ Form::date('PriceDate', \Carbon\Carbon::now(), ['class' => 'form-control']) }}
                             </div>
                         </div>
 
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                {!! Form::label('PriceMax', 'اعلى سعر') !!}
-                                {!! Form::text('PriceMax', $price->PriceMax ?? null, ['class' => 'form-control']) !!}
-                            </div>
-                        </div>
-
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                {!! Form::label('Unit_ID', ' العبـــــوات') !!}
-                                @foreach($units as $id => $unit)
-                                    <div class="col-md-4">
-                                        {!!
-                                            Form::checkbox(
-                                                'Unit_ID[]',
-                                                $id,
-                                                $price->units->contains($id) ?? false
-                                            ) . $unit
-                                        !!}
-                                        {!!
-                                            Form::text(
-                                                'Weights[]',
-                                                $price->getUnitsWeights($id) ?? null,
-                                                ['class' => 'form-control']
-                                            )
-                                        !!}
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                        <div class="col-md-10">
-                            <div class="form-group">
-                                {!! Form::label('Weight', 'الوزن') !!}
-                                {!! Form::text('Weight', $price->Weight ?? null, ['class' => 'form-control']) !!}
-                            </div>
-                        </div>
-
-                        <div class="col-md-10">
-                            <div class="form-group">
-                                {!! Form::label('Desc_A', ' المـواصفات- عربى') !!}
-                                {!! Form::textarea('Desc_A', $price->Desc_A ?? null, ['class' => 'form-control']) !!}
-                            </div>
-                        </div>
-
-                        <div class="col-md-10">
-                            <div class="form-group">
-                                {!! Form::label('Desc_E', 'المـواصفات- انجليزى') !!}
-                                {!! Form::textarea('Desc_E', $price->Desc_E ?? null, ['class' => 'form-control']) !!}
-                            </div>
-                        </div>
-	                    <div class="row">
-                            <div class="col-md-12">
-                                <div class="col-md-6">
-                                    {!! Form::submit('حفظ', ['class' => 'btn btn-primary']) !!}
-                                </div>
-                                <div class="col-md-6">
-                                    {!! Form::submit('اضافة جديد', ['class' => 'btn btn-success btn-block']) !!}
-                                </div>
-                            </div>
-                        </div>
                     {!! Form::close() !!}
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4><b>بيانات التوريد</b></h4>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="hsCode">المحصول</label>
+                            <select name="hsCode" id="hsCode" class="form-control">
+                                <option value="1">اول</option>
+                                <option value="2">تانى</option>
+                                <option value="3">ثالث</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">النوع</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">الكمية المطلوبة</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">التعبئة المطلوبة</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">المواصفات المطلوبة</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">المواصفات المطلوبة</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">الشهادات المطلوبة</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-12">
+                        <label for="file">صورة المنتج</label>
+                        <input name="file" type="file">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4><b>بيانات التوريد</b></h4>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="title">مكان تسليم المحصول</label>
+                            <label><input type="radio" name="optradio">باب المزرعة</label>
+                            <label><input type="radio" name="optradio">مقر المشترى</label>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="hsCode">المحافظة</label>
+                            <select name="hsCode" id="hsCode" class="form-control">
+                                <option value="1">اول</option>
+                                <option value="2">تانى</option>
+                                <option value="3">ثالث</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="hsCode">المنطقة - المركز</label>
+                            <select name="hsCode" id="hsCode" class="form-control">
+                                <option value="1">اول</option>
+                                <option value="2">تانى</option>
+                                <option value="3">ثالث</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="hsCode">القرية - الحى</label>
+                            <select name="hsCode" id="hsCode" class="form-control">
+                                <option value="1">اول</option>
+                                <option value="2">تانى</option>
+                                <option value="3">ثالث</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="title">نوع النقل</label>
+                            <label><input type="radio" name="optradio">عادى</label>
+                            <label><input type="radio" name="optradio">مبرد</label>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="title">معدل التوريد</label>
+                            <label><input type="radio" name="optradio">يومى</label>
+                            <label><input type="radio" name="optradio">اسبوعى</label>
+                            <label><input type="radio" name="optradio">شهرى</label>
+                            <label><input type="radio" name="optradio">أخرى</label>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">السعر المقترح</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">طريقة الدفغ</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="name">تفاصيل أخرى</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name">انجليزى</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="file">صورة المنتج</label>
+                            <input name="file" type="file">
+                        </div>
+                    </div>
+
+                     <div class="row">
+                        <div class="col-md-12">
+                            <div class="col-md-12">
+                                {!! Form::submit('حفظ', ['class' => 'btn btn-primary']) !!}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/markets/index.blade.php
+++ resources/views/markets/index.blade.php
@@ -11,31 +11,19 @@
                 </div>
                 <div class="col-md-12">
                     <div class="form-group">
-                        {!! Form::label('HSCode_ID', 'منتج/ كود HS') !!}
-                        {!! Form::select(
-                                'HSCode_ID',
-                                $hSCodes->prepend('من فضلك اختار', 0),
-                                $price->HSCode_ID ?? null,
-                                ['class' => 'form-control']
-                            ) !!}
+                        <label for="hsCode">منتج/ كود HS</label>
+                        <select name="hsCode" id="hsCode" class="form-control">
+                            <option value="1">اول</option>
+                            <option value="2">تانى</option>
+                            <option value="3">ثالث</option>
+                        </select>
                     </div>
                 </div>
                 <div class="col-md-12">
                     <div class="form-group">
-                        {!! Form::label('Market_ID', 'المدينة/الســــوق') !!}
-                        {!! Form::select(
-                                'Market_ID',
-                                [
-                                    0 => 'من فضلك اختار',
-                                    1 => 'سوق العبور/القاهرة',
-                                    2 => 'سوق الجملة/6 اكتوبر',
-                                    3 => 'سوق الحضرة/الاسكندرية',
-                                    4 => 'سوق الجملة/اسيوط',
-                                    5 => 'سوق الجملة/المنيا',
-                                ],
-                                $price->Market_ID ?? null,
-                                ['class' => 'form-control']
-                            ) !!}
+                        <label for="title">نوع العرض</label>
+                        <label><input type="radio" name="optradio">عرض شراء</label>
+                        <label><input type="radio" name="optradio">عرض بيع</label>
                     </div>
                 </div>
                 <div class="col-md-6">
@@ -54,10 +42,10 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="col-md-6">
-                            {!! Form::submit('بحث', ['class' => 'btn btn-primary btn-block']) !!}
+                            {!! Form::submit('ادخال عروض شراء', ['class' => 'btn btn-primary btn-block']) !!}
                         </div>
                         <div class="col-md-6">
-                            <a href="{{ route('prices.create') }}" class="btn btn-success btn-block">اضافة جديد</a>
+                            <a href="#" class="btn btn-success btn-block">ادخال عروض بيع</a>
                         </div>
                     </div>
                 </div>
@@ -67,51 +55,31 @@
                 <table class="table table-striped">
                     <thead>
                         <tr>
-                            <th>كود</th>
-                            <th>منتج</th>
-                            <th>سوق</th>
-                            <th>تاريخ السعر</th>
-                            <th>سعر السوق</th>
-                            <th>من</th>
-                            <th>الى</th>
-                            <th>المستخدم</th>
-                            <th>الادخال</th>
-                            <th>#</th>
+                            <th>كود العرض</th>
+                            <th>منتج / كود HS</th>
+                            <th>الكمية</th>
+                            <th>تاريخ التسليم</th>
                             <th>#</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse($prices as $price)
                             <tr>
-                               <td>{{ $price->PriceDB_ID }}</td>
-                               <td>{{ $price->hSCode->HS_Aname }}</td>
-                               <td>العبور</td>
-                               <td>{{ $price->PriceDate }}</td>
-                               <td>{{ $price->PriceAverage }}</td>
-                               <td>{{ $price->PriceMin }}</td>
-                               <td>{{ $price->PriceMax }}</td>
-                               <td>{{ $price->updateUser->FullName }}</td>
-                               <td>{{ $price->updated_at }}</td>
+                               <td>10</td>
+                               <td>برتقال صيفى</td>
+                               <td>10</td>
+                               <td>10/12/2016</td>
                                <td>
-                                    <a href="{{ route('prices.edit', $price) }}"
-                                        class="btn btn-sm btn-primary edit">تعديل</a>
-                                </td>
-                                <td>
-                                    {!! Form::open(['method' => 'DELETE', 'route' => ['prices.destroy', $price]]) !!}
-                                        {!! Form::submit('حذف', ['class' => 'btn btn-sm btn-danger']) !!}
-                                    {!! Form::close() !!}
+                                    <a href="#" class="btn btn-sm btn-primary btn-block edit">تفاصيل</a>
                                 </td>
                             </tr>
-                        @empty
                             <tr>
                                 <td colspan="11">لا توجد نتائج لعرضها</td>
                             </tr>
-                        @endforelse
                     </tbody>
                 </table>
-                <div class="text-center">
+                {{-- <div class="text-center">
                     {{ $prices->links() }}
-                </div>
+                </div> --}}
             </div>
         </div>
         @include('layouts.alert')
